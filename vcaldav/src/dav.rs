20189362564f950a975f6c59d46455269e6ffddb
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-02-15 22:25:19 +0100

    Initial commit for vcaldav
    Very initial scaffold for a CalDav client and bootstrap logic (e.g.:
    finding home principal, etc).
    
    Tested so far with Fastmail and Nextcloud.

---
+++ vcaldav/src/dav.rs
@@ -0,0 +1,233 @@
+// A lot of the code is derived from cardamom:
+// https://github.com/soywod/cardamom/
+//
+// Co-Authored-By: Clément DOUIN <clement.douin@posteo.net>
+use serde::Deserialize;
+
+/// A multi-status response (webdav-specific)
+///
+/// ```xml
+/// <multistatus xmlns="DAV:">
+///     <response>
+///         ...
+///     </response>
+///     <response>
+///         ...
+///     </response>
+///     ...
+/// </multistatus>
+/// ```
+///
+/// See: https://www.rfc-editor.org/rfc/rfc2518#section-11
+#[derive(Debug, Deserialize)]
+pub struct Multistatus<T> {
+    #[serde(rename = "response", default)]
+    pub responses: Vec<Response<T>>,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum DavError {
+    #[error("http error executing request")]
+    Network(#[from] reqwest::Error),
+
+    #[error("cannot parse dav response")]
+    Data(#[from] quick_xml::de::DeError),
+
+    #[error("failed to parse a URL returned by the server")]
+    BadUrl(#[from] url::ParseError),
+}
+
+impl<T: for<'a> Deserialize<'a> + Default> Multistatus<T> {
+    /// Convert an HTTP response into a [`Multistatus`].
+    ///
+    /// # Errors
+    ///
+    /// - If streaming the response fails.
+    /// - If the data fails to deserialise as XML.
+    pub(crate) async fn from_response(response: reqwest::Response) -> Result<Self, DavError> {
+        let response = response.error_for_status()?;
+        // TODO: Do we also error for non-207?
+        let body = response.text().await?;
+        let multi_status = quick_xml::de::from_str::<Multistatus<T>>(&body)?;
+
+        Ok(multi_status)
+    }
+}
+
+/// A single response from a [`Multistatus`].
+///
+/// Each response contains a `href` and multiple `propstat`.
+///
+/// ```xml
+/// <response>
+///     <href>/path</href>
+///     <propstat>
+///         ...
+///     </propstat>
+///     <propstat>
+///         ...
+///     </propstat>
+///     ...
+/// <response>
+/// ```
+#[derive(Debug, Deserialize)]
+pub struct Response<T> {
+    pub href: String,
+    #[serde(default)]
+    pub propstat: Vec<Propstat<T>>,
+}
+
+/// A single properties associated to the WebDAV response. The propstat contains
+/// a property `prop` and sometimes a `status` code.
+///
+/// ```xml
+/// <propstat>
+///     <prop>
+///         ...
+///     </prop>
+///     <status>HTTP/1.1 200 OK</status>
+/// </propstat>
+/// ```
+#[derive(Debug, Deserialize)]
+pub struct Propstat<T> {
+    pub prop: T,
+    // From https://www.rfc-editor.org/rfc/rfc2518#section-11:
+    // > contains a set of XML elements called response which contain 200, 300, 400, and 500 series
+    // > status codes generated during the method invocation
+    // TODO: Should probably not use string here.
+    pub status: Option<String>,
+}
+
+/// The current user's principal.
+///
+/// See: https://www.rfc-editor.org/rfc/rfc5397
+#[derive(Debug, Default, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub(crate) struct CurrentUserPrincipalProp {
+    pub current_user_principal: CurrentUserPrincipal,
+}
+
+#[derive(Debug, Default, Deserialize)]
+pub(crate) struct CurrentUserPrincipal {
+    pub href: String,
+}
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub(crate) struct CalendarHomeSetProp {
+    pub calendar_home_set: CalendarHomeSet,
+}
+
+#[derive(Debug, Default, Deserialize)]
+pub(crate) struct CalendarHomeSet {
+    pub href: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use quick_xml::de as xml;
+
+    use super::*;
+
+    #[test]
+    fn empty_response() {
+        let res: Multistatus<String> = xml::from_str(r#"<multistatus xmlns="DAV:" />"#).unwrap();
+        assert_eq!(0, res.responses.len());
+    }
+
+    #[test]
+    fn single_propstat() {
+        let res: Multistatus<String> = xml::from_str(
+            r#"
+            <multistatus xmlns="DAV:">
+                <response>
+                    <href>/path</href>
+                    <propstat>
+                        <prop>data</prop>
+                        <status>HTTP/1.1 200 OK</status>
+                    </propstat>
+                </response>
+            </multistatus>
+            "#,
+        )
+        .unwrap();
+
+        assert_eq!(1, res.responses.len());
+        assert_eq!("/path", res.responses[0].href);
+        assert_eq!(1, res.responses[0].propstat.len());
+        assert_eq!("data", res.responses[0].propstat[0].prop);
+        assert_eq!(
+            Some("HTTP/1.1 200 OK"),
+            res.responses[0].propstat[0]
+                .status
+                .as_ref()
+                .map(|s| s.as_ref())
+        );
+    }
+
+    #[test]
+    fn multiple_propstats() {
+        #[derive(Debug, Default, Deserialize)]
+        struct Response {
+            getetag: Option<String>,
+            getlastmodified: Option<String>,
+        }
+
+        let res: Multistatus<Response> = xml::from_str(
+            r#"
+            <multistatus xmlns="DAV:">
+                <response>
+                    <href>/path</href>
+                    <propstat>
+                        <prop>
+                            <getetag>etag</getetag>
+                        </prop>
+                        <status>HTTP/1.1 200 OK</status>
+                    </propstat>
+                    <propstat>
+                        <prop>
+                            <getlastmodified />
+                        </prop>
+                        <status>HTTP/1.1 404 Not Found</status>
+                    </propstat>
+                </response>
+            </multistatus>
+            "#,
+        )
+        .unwrap();
+
+        assert_eq!(1, res.responses.len());
+        assert_eq!("/path", res.responses[0].href);
+        assert_eq!(2, res.responses[0].propstat.len());
+        assert_eq!(
+            Some("etag"),
+            res.responses[0].propstat[0]
+                .prop
+                .getetag
+                .as_ref()
+                .map(|etag| etag.as_ref())
+        );
+        assert_eq!(
+            Some("HTTP/1.1 200 OK"),
+            res.responses[0].propstat[0]
+                .status
+                .as_ref()
+                .map(|v| v.as_ref())
+        );
+        assert_eq!(
+            Some(""),
+            res.responses[0].propstat[1]
+                .prop
+                .getlastmodified
+                .as_ref()
+                .map(|v| v.as_ref())
+        );
+        assert_eq!(
+            Some("HTTP/1.1 404 Not Found"),
+            res.responses[0].propstat[1]
+                .status
+                .as_ref()
+                .map(|s| s.as_ref())
+        );
+    }
+}
---
+++ vcaldav/src/lib.rs
@@ -0,0 +1,197 @@
+use dav::{CalendarHomeSetProp, DavError};
+use reqwest::{Client, IntoUrl, Method, RequestBuilder, StatusCode};
+use serde::Deserialize;
+use url::Url;
+
+use crate::dav::{CurrentUserPrincipalProp, Multistatus};
+
+pub mod dav;
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub enum Auth {
+    None,
+    Basic {
+        username: String,
+        password: Option<String>,
+    },
+}
+
+// TODO FIXME: Need to figure out how to reuse as much as possible for carddav and caldav.
+#[derive(Debug)]
+pub struct CalDavClient {
+    base_url: Url,
+    auth: Auth,
+    client: Client,
+    context_path: Option<Url>,
+    principal: Option<Url>,
+    calendar_home_set: Option<Url>, // TODO: timeouts
+}
+
+impl CalDavClient {
+    /// Returns a client without any automatic bootstrapping.
+    ///
+    /// It is generally advised to use [`bootstrapped`].
+    pub fn raw_client(base_url: Url, auth: Auth) -> Self {
+        Self {
+            base_url,
+            auth,
+            client: Client::new(),
+            context_path: None,
+            principal: None,
+            calendar_home_set: None,
+        }
+    }
+
+    // TODO: using only a `mailto:` as input would be compliant:
+    //       See: https://www.rfc-editor.org/rfc/rfc6764#section-6
+    //       The Url crate seems to support this.
+
+    /// Returns a bootstrapped client.
+    pub async fn bootstrapped(base_url: Url, auth: Auth) -> Result<Self, DavError> {
+        let mut client = Self::raw_client(base_url, auth);
+        client.bootstrap().await?;
+        Ok(client)
+    }
+
+    // TODO: methods to serialise and deserialise (mostly to cache all the data).
+
+    /// Returns a request with the proper `Authorization` header set.
+    fn request<U: IntoUrl>(&self, method: Method, url: U) -> RequestBuilder {
+        let request = self.client.request(method, url);
+        match &self.auth {
+            Auth::None => request,
+            Auth::Basic { username, password } => request.basic_auth(username, password.as_ref()),
+        }
+    }
+
+    /// Bootstrap this client
+    ///
+    /// Determines the real URL and path of the CalDav resources for a server.
+    ///
+    /// See: https://www.rfc-editor.org/rfc/rfc6764
+    pub async fn bootstrap(&mut self) -> Result<(), DavError> {
+        // TODO: check DNS-SD service labels: https://www.rfc-editor.org/rfc/rfc6764#section-3 (_caldavs._tcp)
+        // TODO: check DNS-SD service labels: https://www.rfc-editor.org/rfc/rfc6764#section-3 (_caldav._tcp)
+        // TODO: check TXT records: https://www.rfc-editor.org/rfc/rfc6764#section-4
+
+        // NOTE: context path may have been defined via TXT record!
+        //       (though if it errors, we should still resolve via well-known.
+        self.context_path = Some(self.resolve_context_path().await?);
+
+        // From https://www.rfc-editor.org/rfc/rfc6764#section-6, subsection 5:
+        // > clients MUST properly handle HTTP redirect responses for the request
+        self.principal = self.resolve_current_user_principal().await?;
+
+        // If there's no principal, it's unclear where to query for the calendar home set.
+        if let Some(principal) = &self.principal {
+            self.calendar_home_set = self.query_calendar_home_set(principal.clone()).await?;
+        }
+
+        // TODO: use the user principal url to exec PROPFIND and discover calendars.
+
+        Ok(())
+    }
+
+    /// Resolve the default context path with the well-known URL.
+    ///
+    /// See: https://www.rfc-editor.org/rfc/rfc6764#section-5
+    pub async fn resolve_context_path(&self) -> Result<Url, reqwest::Error> {
+        let mut url = self.base_url.clone();
+        url.set_path("/.well-known/caldav");
+
+        // From https://www.rfc-editor.org/rfc/rfc6764#section-5:
+        // > [...] the server MAY require authentication when a client tries to
+        // > access the ".well-known" URI
+        self.request(Method::GET, url)
+            .send()
+            .await
+            .map(|resp| resp.url().to_owned())
+        // FIXME: if the URL is the same, we have no context path
+        // TODO: Should actually check that we've gotten 301, 303 or 307.
+        //       The main issue is that reqwest does not allow changing this on a per-request basis.
+        //       Maybe hyper is more adequate here?
+    }
+
+    /// Resolves the current user's principal resource.
+    ///
+    /// See: https://www.rfc-editor.org/rfc/rfc5397
+    pub async fn resolve_current_user_principal(&self) -> Result<Option<Url>, DavError> {
+        // Try querying the provided base url...
+        if let Some(context_path) = &self.context_path {
+            let maybe_principal = self
+                .query_current_user_principal(context_path.clone())
+                .await;
+
+            match maybe_principal {
+                Err(DavError::Network(err)) if err.status() == Some(StatusCode::NOT_FOUND) => {}
+                Err(err) => return Err(err),
+                Ok(Some(p)) => return Ok(Some(p)),
+                Ok(None) => {}
+            };
+        }
+
+        // ... Otherwise, try querying the root path.
+        let mut root = self.base_url.clone();
+        root.set_path("/");
+        self.query_current_user_principal(root).await // This can be Ok(None)
+
+        // NOTE: If not principal is resolved, it needs to be provided interactively
+        //       by the user. We should use base_url in our case maybe...?
+    }
+
+    async fn query_current_user_principal(&self, url: Url) -> Result<Option<Url>, DavError> {
+        self.propfind::<CurrentUserPrincipalProp>(url.clone(), "", "<current-user-principal />")
+            .await?
+            .responses
+            .first()
+            .and_then(|res| res.propstat.first())
+            .map(|propstat| propstat.prop.current_user_principal.href.to_owned())
+            .map(|principal| url.join(principal.as_ref()).map_err(DavError::BadUrl))
+            .transpose()
+    }
+
+    async fn query_calendar_home_set(&self, url: Url) -> Result<Option<Url>, DavError> {
+        self.propfind::<CalendarHomeSetProp>(
+            url.clone(),
+            r#"xmlns:c="urn:ietf:params:xml:ns:caldav""#,
+            "<c:calendar-home-set />",
+        )
+        .await?
+        .responses
+        .first()
+        .and_then(|res| res.propstat.first())
+        .map(|propstat| propstat.prop.calendar_home_set.href.to_owned())
+        .map(|principal| url.join(principal.as_ref()).map_err(DavError::BadUrl))
+        .transpose()
+    }
+
+    /// Sends a `PROPFIND` request and parses the result.
+    async fn propfind<T: for<'a> Deserialize<'a> + Default>(
+        &self,
+        url: Url,
+        extra_ns: &str,
+        prop: &str,
+    ) -> Result<Multistatus<T>, DavError> {
+        let response = self
+            .request(
+                Method::from_bytes(b"PROPFIND").expect("API for HTTP methods is stupid"),
+                url,
+            )
+            .header("Content-Type", "application/xml; charset=utf-8")
+            .header("Depth", "0")
+            .body(format!(
+                r#"
+                <propfind xmlns="DAV:" {extra_ns}>
+                    <prop>
+                        {prop}
+                    </prop>
+                </propfind>
+                "#
+            ))
+            .send()
+            .await?;
+
+        Multistatus::<T>::from_response(response).await
+    }
+}
