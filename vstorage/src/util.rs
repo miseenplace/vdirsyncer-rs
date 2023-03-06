99b641532e6004a8c4249f8c5882a4d2157143de
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-03-06 10:51:45 +0100

    Add pending TODO

--- vstorage/src/util.rs
+++ vstorage/src/util.rs
@@ -8,5 +8,8 @@ use sha2::{Digest, Sha256};
 ///
 /// [`Etag`]: crate::base::Etag
 pub fn hash<S: AsRef<str>>(input: S) -> String {
+    // TODO: vdirsyner-py ignores lines that start with PRODID, since this gets mutated all the
+    // time when entries don't change.
+    // See: hash_item and normalize_item
     format!("{:X}", Sha256::digest(input.as_ref()))
 }
