d961e4aab362a233d7f18bde4c602ba28771d0a4
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-03-08 14:20:40 +0100

    Implement CardDavClient

---
+++ libdav/examples/find_addressbooks.rs
@@ -0,0 +1,75 @@
+//! An example of some basic usage of the `CardDavClient` type.
+//!
+//! Usage:
+//!
+//!     cargo run --example=find_addressbooks https://example.com user@example.com MYPASSWORD
+//!     cargo run --example=find_addressbooks $SERVER_URL         $USERNAME        $PASSWORD
+//!
+//! Example output (with $1 = "https://fastmail.com"):
+//!
+//! ```
+//! Resolved server URL to: https://d277161.carddav.fastmail.com/dav/addressbooks
+//! found 2 addressbooks...
+//! 📇 name: Some("Personal"), path: "/dav/addressbooks/user/vdirsyncer@fastmail.com/Default/"
+//! Href and Etag for components in addressbook:
+//! 📇 name: Some("test-vdirsyncer-ci-bedd62c5-ede3-4e07-87c0-163c259c634f"), path: "/dav/addressbooks/user/vdirsyncer@fastmail.com/test-vdirsyncer-ci-bedd62c5-ede3-4e07-87c0-163c259c634f/"
+//! Href and Etag for components in addressbook:
+//! ```
+use http::Uri;
+use libdav::auth::Auth;
+use libdav::CardDavClient;
+
+#[tokio::main]
+async fn main() {
+    let mut arguments = std::env::args();
+    arguments
+        .next()
+        .expect("binary has been called with a name");
+    let base_url: Uri = arguments
+        .next()
+        .expect("$1 is defined")
+        .parse()
+        .expect("$1 is a valid URL");
+    let username = arguments.next().expect("$2 is a valid username");
+    let password = arguments.next().expect("$3 is a valid password");
+
+    let carddav_client = CardDavClient::auto_bootstrap(
+        base_url,
+        Auth::Basic {
+            username,
+            password: Some(password),
+        },
+    )
+    .await
+    .unwrap();
+
+    println!("Resolved server URL to: {}", carddav_client.context_path());
+
+    let home_set = carddav_client
+        .addressbook_home_set
+        .as_ref()
+        .unwrap()
+        .clone();
+    let addressbooks = carddav_client.find_addresbooks(home_set).await.unwrap();
+
+    println!("found {} addressbooks...", addressbooks.len());
+
+    for ref addressbook in addressbooks {
+        let name = carddav_client
+            .get_collection_displayname(addressbook)
+            .await
+            .unwrap();
+        println!("📇 name: {name:?}, path: {addressbook:?}");
+        let items = carddav_client
+            .list_collection(addressbook)
+            .await
+            .unwrap()
+            .into_iter()
+            .map(Result::unwrap)
+            .filter(|i| !i.prop.is_collection);
+        println!("Href and Etag for components in addressbook:");
+        for item in items {
+            println!("- {}, {}", item.href, item.prop.etag);
+        }
+    }
+}
--- libdav/src/lib.rs
+++ libdav/src/lib.rs
@@ -1,6 +1,6 @@
 //! This library contains caldav and carddav clients.
 //!
-//! See [`CalDavClient`] as a useful entry point.
+//! See [`CalDavClient`] and [`CardDavClient`] as a useful entry points.
 use std::{
     io,
     ops::{Deref, DerefMut},
@@ -229,6 +229,166 @@ impl DavWithAutoDiscovery for CalDavClient {
                 Some(scheme) if scheme == "http" => Ok(80),
                 Some(scheme) if scheme == "caldavs" => Ok(443),
                 Some(scheme) if scheme == "caldav" => Ok(80),
+                _ => Err(BootstrapError::InvalidUrl("invalid scheme (and no port)")),
+            }
+        }
+    }
+
+    fn service(&self) -> Result<DiscoverableService, BootstrapError> {
+        let scheme = self
+            .base_url
+            .scheme()
+            .ok_or(BootstrapError::InvalidUrl("missing scheme"))?;
+        match scheme.as_ref() {
+            "https" | "caldavs" => Ok(DiscoverableService::CalDavs),
+            "http" | "caldav" => Ok(DiscoverableService::CalDav),
+            _ => Err(BootstrapError::InvalidUrl("scheme is invalid")),
+        }
+    }
+
+    fn set_principal(&mut self, principal: Option<Uri>) {
+        self.principal = principal
+    }
+}
+
+#[derive(Debug)]
+pub struct CardDavClient {
+    /// The `base_url` may be (due to bootstrapping discovery) different to the one provided as input.
+    ///
+    /// See: <https://www.rfc-editor.org/rfc/rfc6764#section-1>
+    dav_client: DavClient,
+    /// URL of collections that are either address book collections or ordinary collections
+    /// that have child or descendant address book collections owned by the principal.
+    /// See: <https://www.rfc-editor.org/rfc/rfc4791#section-6.2.1>
+    pub addressbook_home_set: Option<Uri>, // TODO: timeouts
+}
+
+impl Deref for CardDavClient {
+    type Target = DavClient;
+
+    fn deref(&self) -> &Self::Target {
+        &self.dav_client
+    }
+}
+impl DerefMut for CardDavClient {
+    fn deref_mut(&mut self) -> &mut dav::DavClient {
+        &mut self.dav_client
+    }
+}
+
+impl CardDavClient {
+    /// Returns a client without any automatic bootstrapping.
+    ///
+    /// It is generally advised to use [`CardDavClient::auto_bootstrap`] instead.
+    pub fn raw_client(base_url: Uri, auth: Auth) -> Self {
+        // TODO: check that the URL is http or https (or mailto:?).
+
+        Self {
+            dav_client: DavClient::new(base_url, auth),
+            addressbook_home_set: None,
+        }
+    }
+
+    // TODO: methods to serialise and deserialise (mostly to cache all discovery data).
+
+    /// Auto-bootstrap a new client.
+    ///
+    /// Determines the carddav server's real host and the context path of the resources for a
+    /// server, following the discovery mechanism described in [rfc6764].
+    ///
+    /// [rfc6764]: https://www.rfc-editor.org/rfc/rfc6764
+    ///
+    /// # Errors
+    ///
+    /// If any of the underlying DNS or HTTP requests fail, or if any of the responses fail to
+    /// parse.
+    ///
+    /// Does not return an error if DNS records as missing, only if they contain invalid data.
+    pub async fn auto_bootstrap(base_url: Uri, auth: Auth) -> Result<Self, BootstrapError> {
+        let mut client = Self::raw_client(base_url, auth);
+        client = client.common_bootstrap().await?;
+
+        // If obtaining a principal fails, the specification says we should query the user. This
+        // tries to use the `base_url` first, since the user might have provided it for a reason.
+        let principal_url = client
+            .principal
+            .as_ref()
+            .unwrap_or(&client.base_url)
+            .clone();
+        client.addressbook_home_set = client.query_addressbook_home_set(principal_url).await?;
+
+        Ok(client)
+    }
+
+    async fn query_addressbook_home_set(&self, url: Uri) -> Result<Option<Uri>, DavError> {
+        let property_data = SimplePropertyMeta {
+            name: b"addressbook-home-set".to_vec(),
+            namespace: xml::CARDDAV.to_vec(),
+        };
+
+        self.find_href_prop_as_uri(
+            url,
+            "<addressbook-home-set xmlns=\"urn:ietf:params:xml:ns:carddav\"/>",
+            property_data,
+        )
+        .await
+    }
+
+    /// Find address book collections under the given `url`.
+    ///
+    /// Returns absolute paths to each addressbook. This method should be called
+    /// with the `addressbook_home_set` URL to find the current user's address books.
+    ///
+    /// # Errors
+    ///
+    /// If the HTTP call fails or parsing the XML response fails.
+    pub async fn find_addresbooks(&self, url: Uri) -> Result<Vec<String>, DavError> {
+        // FIXME: DRY: This is almost a copy-paste of the same method from CalDavClient
+        Ok(self
+            // XXX: depth 1 or infinity?
+            .propfind::<ResponseWithProp<ItemDetails>>(
+                url.clone(),
+                "<resourcetype/><getetag/>",
+                1,
+                (),
+            )
+            .await
+            .map_err(DavError::from)?
+            .into_iter()
+            .filter(|c| {
+                if let Ok(cal) = c {
+                    cal.prop.is_address_book
+                } else {
+                    true
+                }
+            })
+            // FIXME: silently ignores collections with any issues:
+            .filter_map(|r| r.map(|c| c.href).ok())
+            .collect())
+    }
+
+    // TODO: get_addressbook_description ("addressbook-description", "urn:ietf:params:xml:ns:carddav")
+    // TODO: DRY: the above methods are super repetitive.
+    //       Maybe all these props impl a single trait, so the API could be `get_prop<T>(url)`?
+}
+
+#[async_trait]
+impl DavWithAutoDiscovery for CardDavClient {
+    /// Returns the default port to try and use.
+    ///
+    /// If the `base_url` has an explicit port, that value is returned. Otherwise,
+    /// returns `443` for https, `80` for http, and `443` as a fallback for
+    /// anything else.
+    fn default_port(&self) -> Result<u16, BootstrapError> {
+        // raise InvaidUrl?
+        if let Some(port) = self.base_url.port_u16() {
+            Ok(port)
+        } else {
+            match self.base_url.scheme() {
+                Some(scheme) if scheme == "https" => Ok(443),
+                Some(scheme) if scheme == "http" => Ok(80),
+                Some(scheme) if scheme == "carddavs" => Ok(443),
+                Some(scheme) if scheme == "carddav" => Ok(80),
                 _ => Err(BootstrapError::InvalidUrl("invalid scheme (and no port)")),
             }
         }
