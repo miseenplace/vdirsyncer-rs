b3bd426d189f464b6ef3e315c3f8116fdcec5296
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2023-06-20 20:56:18 +0200

    Add link to previous algorithm

--- vstorage/src/sync.rs
+++ vstorage/src/sync.rs
@@ -2,7 +2,6 @@
 //!
 //! The general gist behind synchronising is:
 //!
-//!
 //! - Create a [`StoragePair`] instance, which has the state saved from the previous sync and the
 //!   two storages that are to be synchronised.
 //! - Create a [`Plan`][plan::Plan] which contains a list of actions to be executed to sync both
@@ -11,6 +10,11 @@
 //! - Run [`Plan::execute`][plan::Plan::execute]. This returns two opaque states that should be
 //!   serialised and used as input for the next synchronisation (mostly, this helps understand when
 //!   an item has change on one side vs where there is a conflict).
+//!
+//! The synchronization algorithm is based on [the algorithm from the original
+//! vdirsyncer][original-algo].
+//!
+//! [original-algo]: https://unterwaditzer.net/2016/sync-algorithm.html
 mod pair;
 pub mod plan;
 
